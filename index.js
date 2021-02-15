1c046f5ce6b7e228b6445450769cac3d80df5a20
Jeffrey <pa022069@gmail.com>
2021-02-15 11:53:42 +0800

    v1.0

--- index.js
+++ index.js
@@ -2,21 +2,31 @@
 let wrapper = document.getElementById("box");
 let wrapperInfo = wrapper.getBoundingClientRect();
 
+
+
 // 物件數據
-let item = document.getElementById("item");
+let item = document.querySelectorAll(".item")[1];
 let itemInfo = item.getBoundingClientRect();
+let borderWidth = 1 * 2;
+
 let itemOrigin = {
     x: itemInfo.x,
     y: itemInfo.y
 }
 
-let originPos;
-let movingPos;
+let originPos = {
+    x: 0,
+    y: 0
+};
+let movingPos = {
+    x: 0,
+    y: 0
+};
 let startPos = {
     x: 0,
     y: 0
 };
-let borderWidth = 1 * 2;
+
 
 function dragDownItem(e) {
     e = e || window.event;
@@ -40,7 +50,7 @@ function moveItem(e) {
         y: startPos.y + e.clientY - originPos.y
     }
 
-    let wrapperWidth = wrapperInfo.width - itemInfo.width - borderWidth;
+    let wrapperWidth = wrapperInfo.width - itemInfo.width;
 
     if (movingPos.x >= 0 && movingPos.x <= wrapperWidth) {
         item.style.left = `${movingPos.x}px`;
@@ -49,10 +59,10 @@ function moveItem(e) {
         item.style.left = `0px`;
     }
     if (movingPos.x > wrapperWidth) {
-        item.style.left = `${wrapperWidth}px`;
+        item.style.left = `${wrapperWidth - borderWidth}px`;
     };
 
-    let wrapperHeight = wrapperInfo.height - itemInfo.height - borderWidth;
+    let wrapperHeight = wrapperInfo.height - itemInfo.height;
 
     if (movingPos.y >= 0 && movingPos.y <= wrapperHeight) {
         item.style.top = `${movingPos.y}px`;
@@ -61,7 +71,7 @@ function moveItem(e) {
         item.style.top = `0px`;
     }
     if (movingPos.y > wrapperHeight) {
-        item.style.top = `${wrapperHeight}px`;
+        item.style.top = `${wrapperHeight - borderWidth}px`;
     };
 }
 
@@ -81,18 +91,24 @@ let controller = document.querySelectorAll('span');
 for (let target of controller) {
     target.onmousedown = controlItem;
 }
-
+let minSize = 40;
 let resizeOrigin;
 let resizeMoving;
 let resizeTarget;
 
+let isReverse = {
+    x: false,
+    y: false
+}
+
 function closeResize() {
     itemInfo = item.getBoundingClientRect();
-    startPos = {
+    movingPos = {
         x: itemInfo.x - itemOrigin.x,
         y: itemInfo.y - itemOrigin.y
     }
     item.onmousedown = dragDownItem;
+
     document.onmouseup = null;
     document.onmousemove = null;
 }
@@ -116,59 +132,103 @@ function controlResize(e) {
     e = e || window.event;
     e.preventDefault();
 
-    resizeMoving = {
+    let resizing = {
         x: e.clientX - resizeOrigin.x,
         y: e.clientY - resizeOrigin.y
     }
 
-    let newWidth = startPos.x + itemInfo.width + resizeMoving.x;
-    let newHeight = startPos.y + itemInfo.height + resizeMoving.y;
+    resizeMoving = {
+        x: isReverse.x ? -(resizing.x) : resizing.x,
+        y: isReverse.y ? -(resizing.y) : resizing.y,
+    }
 
+    let newResize = {
+        width: resizeMoving.x + itemInfo.width,
+        height: resizeMoving.y + itemInfo.height,
+        x: startPos.x + resizeMoving.x,
+        y: startPos.y + resizeMoving.y
+    }
+    
     // 尺寸
-    function setResizeWidth(_direct = '') {
-        if (newWidth <= wrapperInfo.width && item.clientWidth >= 40) {
-            item.style.width = `${itemInfo.width + (_direct === "reverse" ? -(resizeMoving.x) : resizeMoving.x)}px`;
+    function setResizeWidth() {
+        if(startPos.x < 0) return;
+        if (newResize.width + startPos.x <= wrapperInfo.width && item.clientWidth >= minSize) {
+            item.style.width = `${itemInfo.width + resizeMoving.x - 2}px`;
         }
-        if (newWidth > wrapperInfo.width) {
+        if (newResize.width + startPos.x > wrapperInfo.width) {
             item.style.width = `${wrapperInfo.width - 4 - startPos.x}px`;
         }
-        if (item.clientWidth < 40) {
-            item.style.width = `40px`
+        if (item.clientWidth < minSize) {
+            item.style.width = `${minSize}px`
         }
     }
-    function setResizeHeight(_direct = '') {
-        if (newHeight <= wrapperInfo.height && item.clientHeight >= 40) {
-            item.style.height = `${itemInfo.height + (_direct === "reverse" ? -(resizeMoving.y) : resizeMoving.y)}px`;
+    function setResizeHeight() {
+        if(startPos.y < 0) return;
+        if (newResize.height + startPos.y <= wrapperInfo.height && item.clientHeight >= minSize) {
+            item.style.height = `${itemInfo.height + resizeMoving.y - 2}px`;
         }
-        if (newHeight > wrapperInfo.height) {
+        if (newResize.height + startPos.y > wrapperInfo.height) {
             item.style.height = `${wrapperInfo.height - 4 - startPos.y}px`;
         }
-        if (item.clientHeight < 40) {
-            item.style.height = `40px`
+        if (item.clientHeight < minSize) {
+            item.style.height = `${minSize}px`
         }
     }
 
     // 座標
-    // function setResizeX() {
-    //     if (newWidth <= wrapperInfo.width && item.clientWidth >= 40) {
-    //         item.style.left = `${movingPos.x + resizeMoving.x}px`;
-    //     }
-    // }
-    // function setResizeY() {
-    //     if (newHeight <= wrapperInfo.height && item.clientHeight >= 40) {
-    //         item.style.top = `${movingPos.y + resizeMoving.y}px`;
-    //     }
-    // }
-
-    if (resizeTarget === 'rb') {
-        setResizeWidth()
-        setResizeHeight()
+    function setResizeX() {
+        if (!isReverse.x) return;
+        startPos.x = movingPos.x - resizeMoving.x;
+        if (startPos.x > 0) {
+            if (item.clientWidth <= minSize) return;
+            item.style.left = `${startPos.x}px`;
+        } 
+        if (startPos.x < 0) {
+            item.style.left = `0px`;
+        }
+    }
+    function setResizeY() {
+        if (!isReverse.y) return;
+        startPos.y = movingPos.y - resizeMoving.y;
+        if (startPos.y > 0) {
+            if (item.clientHeight <= minSize) return;
+            item.style.top = `${startPos.y}px`;
+        } 
+        if (startPos.y < 0) {
+            item.style.top = `0px`;
+        }
     }
 
-    // if (resizeTarget === 'lt') {
-    //     setResizeWidth("reverse")
-    //     setResizeHeight("reverse")
-    //     setResizeX()
-    //     setResizeY()
-    // }
+    if (resizeTarget === 'lt') {
+        isReverse = {
+            x: true,
+            y: true
+        }
+    }
+
+    if (resizeTarget === 'rt') {
+        isReverse = {
+            x: false,
+            y: true
+        }
+    }
+
+    if (resizeTarget === 'lb') {
+        isReverse = {
+            x: true,
+            y: false
+        }
+    }
+
+    if (resizeTarget === 'rb') {
+        isReverse = {
+            x: false,
+            y: false
+        }
+    }
+
+    setResizeWidth()
+    setResizeHeight()
+    setResizeX()
+    setResizeY()
 }
